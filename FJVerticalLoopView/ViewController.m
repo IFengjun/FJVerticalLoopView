bd0cc58c912e3c751d6b67ecd44c14e21f64fefd
fengjun <fengjun@iwaimai.baidu.com>
2017-01-24 11:12:04 +0800

    update
    fill content

--- FJVerticalLoopView/ViewController.m
+++ FJVerticalLoopView/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "UIVerticalLoopView.h"
 
-@interface ViewController ()
+@interface ViewController ()<VerticalLoopDelegate>
 
 @end
 
@@ -16,9 +17,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSArray *tickerArrs = @[@[@"揽客宝更新升级啦",@"顾客少，我这有你秘笈"],@[@"想要吐槽快升级吧",@"您有新订单，请注意查收"],@[@"放不开了方便",@"666666666"]];
+    
+    UIVerticalLoopView *verticalLoopV = [[UIVerticalLoopView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.height, 50)];
+    verticalLoopV.loopDelegate = self;
+    verticalLoopV.backgroundColor = [UIColor whiteColor];
+    verticalLoopV.verticalLoopContentArr = tickerArrs;
+    verticalLoopV.verticalLoopAnimationDuration = 1;
+    verticalLoopV.Direction = VerticalLoopDirectionDown;
+    [self.view addSubview:verticalLoopV];
+    [verticalLoopV start];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)didClickContentAtIndex:(NSInteger)index{
+    
+    NSLog(@"%ld",(long)index);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
